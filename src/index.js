90276baf6658acf39b042ada744ccf83e2bdfd0f
Yu Sun <usunyu@gmail.com>
2018-07-24 14:21:30 +0800

    Add react-router package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/app';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import store from './store';
+
+import App from './components/app';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={App}/>
+            </Switch>
+        </BrowserRouter>
     </Provider>
     , document.querySelector('.container'));
